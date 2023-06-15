4919dcf771ef0fcc8589a7bb609f9496c4dc016e
Gábor Farkas <farkas.gz@gmail.com>
2023-06-15 15:18:33 +0200

    still loving js

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,24 +1,59 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  }
+  return num2;
+}
 
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+  if (words.length === 0){return null;};
+  let longestIndex = 0;
+  for (let i=0; i < words.length; ++i){
+    if (words[i].length > words[longestIndex].length){
+      longestIndex = i;
+    }
+  }
+  return words[longestIndex];
+}
 
 
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+  if (numbers.length === 0){return 0;};
+  sum = 0;
+  for(let i=0; i < numbers.length; ++i){
+    sum += numbers[i];
+  }
+  return sum;
+}
 
 
 
 // Iteration #3.1 Bonus:
-function sum() {}
+function sum(array){
+  if (array.length === 0){return 0;}
+  let sum = 0;
+  for (let i=0; i < array.length; ++i){
+    if(typeof array[i] === "object"){
+      throw new Error("Unsupported data type sir or ma'am");
+    }
+    if(typeof array[i] === "string"){
+      sum += array[i].length;
+    }else {
+      sum += array[i];
+    }
+  }
+  return sum;
+}
 
 
 
@@ -26,16 +61,41 @@ function sum() {}
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbers) {
+  if (numbers.length === 0){return null;}
+  let sum = 0;
+  for (let i=0; i < numbers.length; ++i){
+    sum += numbers[i];
+  }
+  return sum/numbers.length;
+}
 
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 
-function averageWordLength() { }
+function averageWordLength(words) {
+  if (words.length === 0){return null;}
+  let sum = 0;
+  for (let i=0; i < words.length; ++i){
+    sum += words[i].length;
+  }
+  return sum/words.length;
+}
 
 // Bonus - Iteration #4.1
-function avg() {}
+function avg(array) {
+  if (array.length === 0){return null;}
+  let sum = 0;
+  for (let i=0; i < array.length; ++i){
+    if(typeof array[i] === "string"){
+      sum += array[i].length;
+    }else {
+      sum += array[i];
+    }
+  }
+  return sum/array.length;
+}
 
 // Iteration #5: Unique arrays
 const wordsUnique = [
@@ -52,14 +112,29 @@ const wordsUnique = [
   'bring'
 ];
 
-function uniquifyArray() {}
+function uniquifyArray(words) {
+  if (words.length === 0){return null;}
+  let uniqueArray = [];
+  for (let i = 0; i < words.length; ++i){
+    if (uniqueArray.includes(words[i]) === false){
+      uniqueArray.push(words[i]);
+    }
+  }
+  return uniqueArray;
+}
 
 
 
 // Iteration #6: Find elements
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
 
-function doesWordExist() {}
+function doesWordExist(arrayOfWords, word) {
+  if (arrayOfWords.length === 0){return null;}
+  for (let i=0; i < arrayOfWords.length; ++i){
+    if (arrayOfWords[i] === word){return true;}
+  }
+  return false;
+}
 
 
 
@@ -78,7 +153,16 @@ const wordsCount = [
   'matter'
 ];
 
-function howManyTimes() {}
+function howManyTimes(arrayOfWords, word) {
+  if (arrayOfWords.length === 0){return 0;}
+  let counter = 0;
+  for (let i=0; i < arrayOfWords.length; ++i){
+    if (arrayOfWords[i] === word){
+      ++counter;
+    }
+  }
+  return counter;
+}
 
 
 
@@ -106,7 +190,18 @@ const matrix = [
   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ];
 
-function greatestProduct() {}
+function greatestProduct(matrix) {
+  let greatestProduct = 0;
+  for (let i=0; i < matrix.length-1; ++i){
+    for (let j = 0; j < matrix[i].length-1; ++j){
+      const product = matrix[i][j]*matrix[i][j+1]*matrix[i+1][j]*matrix[i+1][j+1];
+      if(product > greatestProduct){
+        greatestProduct=product;
+      }
+    }
+  }
+  return greatestProduct;
+}
 
 
 
